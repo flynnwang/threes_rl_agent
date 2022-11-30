b9bb68c03fa263a91a5739c4878ed87a066c9da8
Fei Wang <wang.flynn@gmail.com>
2022-11-30 22:12:44 +0800

    add camera support

--- tools/camera_app.py
+++ tools/camera_app.py
@@ -9,16 +9,31 @@ app = Flask(__name__)
 
 TEST_IMG_PATH = "/Users/flynn.wang/repo/flynn/thress_imgs/record_1124/002a1a0c-bd7d-4d36-ac1c-fdf5f7d2fdfb.jpg"
 
+DEBUG = True
 
-def get_test_img_binary():
-  with open(TEST_IMG_PATH, 'rb') as f:
-    return io.BytesIO(f.read())
+if not DEBUG:
+  import picamera
+  camera = picamera.PiCamera()
+  camera.resolution = (2592, 1944)
+  # Start a preview and let the camera warm up for 2 seconds
+  camera.start_preview()
+  time.sleep(2)
+
+
+def capture_image():
+  if DEBUG:
+    with open(TEST_IMG_PATH, 'rb') as f:
+      return io.BytesIO(f.read())
+
+  img = BytesIO()
+  camera.capture(img, 'jpeg')
+  return img
 
 
 @app.route("/board.jpg")
 def get_img():
-  img_file = get_test_img_binary()
-  return send_file(img_file,
+  img = capture_image()
+  return send_file(img,
                    mimetype='image/jpeg',
                    as_attachment=True,
                    download_name='board.jpg')
